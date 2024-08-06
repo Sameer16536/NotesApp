16d5b92af30d0363a9dddacbbab0b425831bb528
Sameer <sameer.marathe16536@sakec.ac.in>
2024-08-06 09:25:17 +0530

    few changes

---
+++ frontend/src/components/Docs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Docs:React.FC = () => {
+  return (
+    <div className='docs-main'>
+        <h1>Docs</h1>
+        <button className='docs-button'>Add Document</button>
+    </div>
+  )
+}
+
+export default Docs
